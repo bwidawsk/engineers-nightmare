a8ed997e5b99c2fde3cccb34b28eb63aec1a68c4
Chris Forbes <chrisf@ijw.co.nz>
2015-09-03 04:59:15 +1200

    NOMERGE: experiment in generic resize of pool
    Signed-off-by: Chris Forbes <chrisf@ijw.co.nz>

--- src/component/relative_position.cc
+++ src/component/relative_position.cc
@@ -4,34 +4,73 @@
 #include "../memory.h"
 #include "relative_position.h"
 
+
+struct component_member {
+    unsigned align, size;
+};
+
+static void
+resize_pool(unsigned count,
+            component_manager::component_buffer *buffer,
+            void ** old_pool, void ** new_pool,
+            unsigned member_count, component_member const *members) {
+
+    component_manager::component_buffer new_buffer;
+
+    size_t size = 0;
+    unsigned worst_misalign = 0;
+
+    for (auto i = 0u; i < member_count; i++) {
+        worst_misalign = std::max(members[i].align, worst_misalign);
+        size = align_size(size, members[i].align) + members[i].size * count;
+    }
+
+    size += worst_misalign;
+
+    new_buffer.buffer = malloc(size);
+    new_buffer.num = buffer->num;
+    new_buffer.allocated = count;
+    memset(new_buffer.buffer, 0, size);
+
+    size_t off = (size_t)new_buffer.buffer;
+    for (auto i = 0u; i < member_count; i++) {
+        off = align_size(off, members[i].align);
+        new_pool[i] = (void *)off;
+        off += members[i].size * count;
+    }
+
+    if (buffer->allocated) {
+        for (auto i = 0u; i < member_count; i++) {
+            memcpy(new_pool[i], old_pool[i], buffer->num * members[i].size);
+        }
+        free(buffer->buffer);
+    }
+
+    *buffer = new_buffer;
+}
+
+
+#define ARRAY_SIZE(x)   (sizeof(x)/sizeof(*x))
+#define DECLARE_MEMBER(x)       { alignof(x), sizeof(x) }
+
+
+component_member relative_position_desc[] = {
+    DECLARE_MEMBER(c_entity),
+    DECLARE_MEMBER(glm::vec3),
+    DECLARE_MEMBER(glm::mat4)
+};
+
+
 void
 relative_position_component_manager::create_component_instance_data(unsigned count) {
     if (count <= buffer.allocated)
         return;
 
-    component_buffer new_buffer;
     relative_position_instance_data new_pool;
 
-    size_t size = sizeof(c_entity) * count;
-    size = sizeof(glm::vec3) * count + align_size<glm::vec3>(size);
-    size = sizeof(glm::mat4) * count + align_size<glm::mat4>(size);
-    size += alignof(glm::mat4); // for worst-case misalignment of initial ptr
-
-    new_buffer.buffer = malloc(size);
-    new_buffer.num = buffer.num;
-    new_buffer.allocated = count;
-    memset(new_buffer.buffer, 0, size);
-
-    new_pool.entity = align_ptr((c_entity *)new_buffer.buffer);
-    new_pool.position = align_ptr((glm::vec3 *)(new_pool.entity + count));
-    new_pool.mat = align_ptr((glm::mat4 *)(new_pool.position + count));
-
-    memcpy(new_pool.entity, instance_pool.entity, buffer.num * sizeof(c_entity));
-    memcpy(new_pool.position, instance_pool.position, buffer.num * sizeof(glm::vec3));
-    memcpy(new_pool.mat, instance_pool.mat, buffer.num * sizeof(glm::mat4));
-
-    free(buffer.buffer);
-    buffer = new_buffer;
+    resize_pool(count, &buffer, (void **)&instance_pool, (void **)&new_pool,
+                ARRAY_SIZE(relative_position_desc),
+                relative_position_desc);
 
     instance_pool = new_pool;
 }
--- src/memory.h
+++ src/memory.h
@@ -1,11 +1,23 @@
 #pragma once
 
+static inline
+size_t align_size(size_t s, size_t align)
+{
+    s += align-1;
+    s &= ~(align-1);
+    return s;
+}
+
+static inline
+void * align_ptr(void *p, size_t align)
+{
+    return (void *)align_size((size_t)p, align);
+}
+
 template<typename T>
 size_t align_size(size_t s)
 {
-    s += alignof(T)-1;
-    s &= ~(alignof(T)-1);
-    return s;
+    return align_size(s, alignof(T));
 }
 
 template<typename T>
